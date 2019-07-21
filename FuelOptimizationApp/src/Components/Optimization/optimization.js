43ae4599f03e7d4cf5581e52918a761da0d2913f
GlebGlushko <gleb.glushko10@gmail.com>
2019-07-21 15:01:46 +0300

    added initilizing for start and finish points

--- FuelOptimizationApp/src/Components/Optimization/optimization.js
+++ FuelOptimizationApp/src/Components/Optimization/optimization.js
@@ -11,13 +11,38 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
 import './optimization.css'
 
+const necesseryPoints = [{
+    name: '',
+    startOrFinish: 1,
+    orders: null,
+    coordinates: null,
+    // locationId: 'NT_s5uXPmqbNH4pCOzMAorV.C',
+    locationName: '',
+    // MapView: {
+    //     BottomRight: { Latitude: 53.11653, Longitude: 13.90612 },
+    //     TopLeft: { Latitude: 53.1228, Longitude: 13.90277 }
+    // }
+
+},
+{
+    name: '',
+    startOrFinish: -1,
+    orders: null,
+    coordinates: null,
+    // locationId: 'NT_Bn2nZIGG5u7l6Vv2n9z9AD',
+    locationName: '',
+    // MapView: {
+    //     BottomRight: { Latitude: 53.11653, Longitude: 13.90612 },
+    //     TopLeft: { Latitude: 53.1228, Longitude: 13.90277 }
+    // }
+}];
 
 class Optimization extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedTruck: {value: 1, label:"Lohr 1.21"},
-            points: null,
+            selectedTruck: { value: 1, label: "Lohr 1.21" },
+            points: necesseryPoints,
             isDialogOpened: false,
             credentials: {
                 app_id: 'fLR4pqJX0jZZZle8nwaM',
@@ -27,65 +52,65 @@ class Optimization extends React.Component {
         }
 
     }
-    componentDidMount() {
-        this.setState({
-            points: [
-                {
-                    name: 'Ukraine',
-                    startOrFinish: 1,
-                    orders: [
-                        {
-                            selectedBrand: { value: 1, label: 'Renault' },
-                            selectedModel: { value: 1, label: 'Logan' },
-                            number: 4
-                        },
-                    ],
-                    coordinates: [50.393219, 30.488314],
-                    // locationId: 'NT_s5uXPmqbNH4pCOzMAorV.C',
-                    locationName: 'Ukraine',
-                    MapView: {
-                        BottomRight: {Latitude: 53.11653, Longitude: 13.90612},
-                        TopLeft: {Latitude: 53.1228, Longitude: 13.90277}
-                    }
+    // componentDidMount() {
+    //     this.setState({
+    //         points: [
+    //             {
+    //                 name: 'Ukraine',
+    //                 startOrFinish: 1,
+    //                 orders: [
+    //                     {
+    //                         selectedBrand: { value: 1, label: 'Renault' },
+    //                         selectedModel: { value: 1, label: 'Logan' },
+    //                         number: 4
+    //                     },
+    //                 ],
+    //                 coordinates: [50.393219, 30.488314],
+    //                 // locationId: 'NT_s5uXPmqbNH4pCOzMAorV.C',
+    //                 locationName: 'Ukraine',
+    //                 MapView: {
+    //                     BottomRight: { Latitude: 53.11653, Longitude: 13.90612 },
+    //                     TopLeft: { Latitude: 53.1228, Longitude: 13.90277 }
+    //                 }
 
-                },
-                {
-                    name: 'England',
-                    startOrFinish: -1,
-                    orders: [
-                        {
-                            selectedBrand: { value: 2, label: 'Nissan' },
-                            selectedModel: { value: 3, label: 'X-Trail' },
-                            number: 2
-                        },
-                    ],
-                    coordinates: [51.797901, 11.198762],
-                    // locationId: 'NT_Bn2nZIGG5u7l6Vv2n9z9AD',
-                    locationName: 'England',
-                    MapView: {
-                        BottomRight: {Latitude: 53.11653, Longitude: 13.90612},
-                        TopLeft: {Latitude: 53.1228, Longitude: 13.90277}
-                    }
-                }
-            ]
-        });
+    //             },
+    //             {
+    //                 name: 'England',
+    //                 startOrFinish: -1,
+    //                 orders: [
+    //                     {
+    //                         selectedBrand: { value: 2, label: 'Nissan' },
+    //                         selectedModel: { value: 3, label: 'X-Trail' },
+    //                         number: 2
+    //                     },
+    //                 ],
+    //                 coordinates: [51.797901, 11.198762],
+    //                 // locationId: 'NT_Bn2nZIGG5u7l6Vv2n9z9AD',
+    //                 locationName: 'England',
+    //                 MapView: {
+    //                     BottomRight: { Latitude: 53.11653, Longitude: 13.90612 },
+    //                     TopLeft: { Latitude: 53.1228, Longitude: 13.90277 }
+    //                 }
+    //             }
+    //         ]
+    //     });
 
-        fetch('http://localhost:1984/api/GetBrands', {
-            method: 'GET',
-            mode: 'cors',
-        })
-            .then(response=>  response.json() )
-            .then(data=>{
-                this.setState({cars:data});
-                // this.state.cars = data;
-            })
-        // const cars = require('./models.json');
-        // const brands = [...new Set(cars.map((model)=>{
-        //     return model.brandName;
-        // }))];
-        // const models = cars.filter(model=>this.props.selectedBrand && model.brandName === this.props.selectedBrand.label);
-        
-    }
+    //     fetch('http://localhost:1984/api/GetBrands', {
+    //         method: 'GET',
+    //         mode: 'cors',
+    //     })
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             this.setState({ cars: data });
+    //             // this.state.cars = data;
+    //         })
+    //     // const cars = require('./models.json');
+    //     // const brands = [...new Set(cars.map((model)=>{
+    //     //     return model.brandName;
+    //     // }))];
+    //     // const models = cars.filter(model=>this.props.selectedBrand && model.brandName === this.props.selectedBrand.label);
+
+    // }
     handleTruckChanged = (truck) => {
         this.setState({ selectedTruck: truck });
     }
@@ -130,7 +155,7 @@ class Optimization extends React.Component {
     }
     handlePointAdded = () => {
         this.setState({
-            points: [...this.state.points.slice(0,this.state.points.length-1), {
+            points: [...this.state.points.slice(0, this.state.points.length - 1), {
                 startOrFinish: 0,
                 name: '',
                 coordinates: null,
@@ -141,8 +166,8 @@ class Optimization extends React.Component {
                         number: 1
                     },
                 ]
-            }, 
-            this.state.points[this.state.points.length-1]]
+            },
+            this.state.points[this.state.points.length - 1]]
         });
     }
     handlePointDeleted = (pointId) => {
@@ -153,14 +178,14 @@ class Optimization extends React.Component {
         });
     }
     handlePointSelected = (pointId, coordinates) => {
-        const {app_id, app_code} = this.state.credentials;
+        const { app_id, app_code } = this.state.credentials;
         let url = 'https://reverse.geocoder.api.here.com/6.2/reversegeocode.json';
         return fetch(`${url}?prox=${coordinates[0]},${coordinates[1]}&mode=retrieveAddresses&maxresults=1&app_id=${app_id}&app_code=${app_code}`)
             .then(response => {
                 return (response.json());
             }).then(json => {
                 if (json.Response.View.length === 0) return this.state.points[pointId].locationName;
-                
+
                 let res = json.Response.View[0].Result[0].Location.Address;
                 this.setState({
                     points: [...this.state.points.slice(0, pointId),
@@ -175,31 +200,29 @@ class Optimization extends React.Component {
                 return res.Label;
             });
     }
-    handleSendRequested = () =>{
+    handleSendRequested = () => {
         let points = this.state.points;
         let filled = this.state.selectedTruck ? true : false;
-        
-        for (let i=0; i<points.length;++i)
-            {
-                if (!points[i].coordinates||!points[i].orders) 
-                    { 
-                        filled=false; 
-                        break; 
-                    }
-                for(let j=0;j<points[i].orders.length;++j)
-                    if (!points[i].orders[j].selectedBrand || !points[i].orders[j].selectedModel) {
-                        filled = false;
-                        break;
-                    }
 
+        for (let i = 0; i < points.length; ++i) {
+            if (!points[i].coordinates || !points[i].orders) {
+                filled = false;
+                break;
             }
-        
-        if (!filled) this.setState({isDialogOpened:true}); else{
+            for (let j = 0; j < points[i].orders.length; ++j)
+                if (!points[i].orders[j].selectedBrand || !points[i].orders[j].selectedModel) {
+                    filled = false;
+                    break;
+                }
+
+        }
+
+        if (!filled) this.setState({ isDialogOpened: true }); else {
             console.log('ok')
         }
     }
-    handleCloseDialog = () =>{
-        this.setState({isDialogOpened: false});
+    handleCloseDialog = () => {
+        this.setState({ isDialogOpened: false });
     }
     render() {
         return (
@@ -208,7 +231,7 @@ class Optimization extends React.Component {
                 <Route
                     points={this.state.points}
                     credentials={this.state.credentials}
-                    cars = {this.state.cars}
+                    cars={this.state.cars}
                     handleOrderChanged={this.handleOrderChanged}
                     handleOrderAdded={this.handleOrderAdded}
                     handleOrderDeleted={this.handleOrderDeleted}
@@ -218,21 +241,21 @@ class Optimization extends React.Component {
                 // handleLocationIdChanged = {this.handleLocationIdChanged}
                 />
                 <div>
-                    <Fab className ='add-point-icon' size="small" onClick = {this.handlePointAdded}>
-                        <AddIcon/>
+                    <Fab className='add-point-icon' size="small" onClick={this.handlePointAdded}>
+                        <AddIcon />
                     </Fab>
-                    <Fab variant = 'extended'  size = 'medium' className='send-button' onClick = {this.handleSendRequested}>
-                        Send 
+                    <Fab variant='extended' size='medium' className='send-button' onClick={this.handleSendRequested}>
+                        Send
                     </Fab>
                 </div>
-                <Dialog open = {this.state.isDialogOpened}>
+                <Dialog open={this.state.isDialogOpened}>
                     <DialogTitle> Warning </DialogTitle>
                     <DialogContent>
                         <DialogContentText>
                             You have unfilled fields, check your input and try again
                         </DialogContentText>
                         <DialogActions>
-                            <Button onClick = {this.handleCloseDialog}>OK</Button>
+                            <Button onClick={this.handleCloseDialog}>OK</Button>
                         </DialogActions>
                     </DialogContent>
                 </Dialog>
