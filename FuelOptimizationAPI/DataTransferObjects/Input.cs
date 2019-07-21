b2a91147a5211f47d444ca478309dbdc46974934
GlebGlushko <gleb.glushko10@gmail.com>
2019-07-21 14:37:30 +0300

    changed input weights to cars, added output class

--- FuelOptimizationAPI/DataTransferObjects/Input.cs
+++ FuelOptimizationAPI/DataTransferObjects/Input.cs
@@ -15,12 +15,18 @@ namespace DataTransferObjects
         public int DistanceToNextPoint {get; set;}
         public double? FuelCost { get; set; }       //depends if we use service with all fuel costs or we save costs localy,
                                                     //null if PointType == Dealer, but it's possible that we can fuel up at dealer like at terminal
-        public List<int> Weights { get; set; }
-
+        //public List<int> Weights { get; set; }
+        public List<Car> Cars { get; set; }
     }
     public enum PointType
     {
         FuelStation,
         Dealer
+    }
+    public class Car
+    {
+        public int BrandId { get; set; }
+        public int ModelId { get; set; }
+        public int Weight { get; set; }
     }
 }
---
+++ FuelOptimizationAPI/DataTransferObjects/Output.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataTransferObjects
+{
+    public class Output
+    {
+        List<OutputPoint> Points { get; set; }
+
+    }
+    public class OutputPoint
+    {
+        public double[,] Coordinates { get; set; }
+        public List<Car> UnloadCars { get; set; }
+        public double FuelCost { get; set; }
+        public int FuelVolume { get; set; }
+
+    }
+
+}
