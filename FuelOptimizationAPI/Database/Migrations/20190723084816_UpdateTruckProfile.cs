989c8eb541d728aed551aa2bee5d66102b316f11
GlebGlushko <gleb.glushko10@gmail.com>
2019-07-23 11:55:45 +0300

    updated truck profile, added tank capacity, adjustive index

---
+++ FuelOptimizationAPI/Database/Migrations/20190723084816_UpdateTruckProfile.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class UpdateTruckProfile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "FuelConsumptionPerKm",
+                table: "Trucks",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TankCapacity",
+                table: "Trucks",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "WeightAdjustiveConsumptionIndex",
+                table: "Trucks",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FuelConsumptionPerKm",
+                table: "Trucks");
+
+            migrationBuilder.DropColumn(
+                name: "TankCapacity",
+                table: "Trucks");
+
+            migrationBuilder.DropColumn(
+                name: "WeightAdjustiveConsumptionIndex",
+                table: "Trucks");
+        }
+    }
+}
--- FuelOptimizationAPI/Services/Optimization.cs
+++ FuelOptimizationAPI/Services/Optimization.cs
@@ -1,18 +1,31 @@
 ﻿using Services.Interfaces;
 using DataTransferObjects;
 using OptimizationSimplexMethod;
+using System.Linq;
 
 namespace Services
 {
     class Optimization : IOptimization
     {
+        readonly IdbData _dbData;
+        public Optimization(IdbData dbData)
+        {
+            _dbData = dbData;
+        }
+
         public void StartOptimization(Input input)
         {
             int[] volumes = new int[] { 300, 200, 300 };
             double[] costs = new double[] { 1, 1.25, 0.95 };
-            
-            int tank = 600;
+
+            int tank = _dbData.GetTrucks().Find(truck => truck.Id == input.TruckId).TankCapacity;
+
+
             FuelPlane P = new FuelPlane(costs, volumes, tank);
+        }
+        private void CalcVolume()
+        {
+
         }
     }
 }
