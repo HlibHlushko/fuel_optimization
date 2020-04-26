e95eb4287695b00094c99d2725f317d3fc6b3eb8
GlebGlushko <gleb.glushko10@gmail.com>
2020-04-26 23:03:38 +0300

    add http nginx, fix "prety"

--- src/Fleet.UI/src/pages/Usage/Usage.js
+++ src/Fleet.UI/src/pages/Usage/Usage.js
@@ -8,7 +8,7 @@ export class Usage extends React.Component {
         <Typography variant='h5'>Usage</Typography>
         <Typography>This app is intended to build route with the least cost of fuel.</Typography>
         <Typography>It considers such parameters as fuel cost in different countries and car fuel consumption.</Typography>
-        <Typography>So flow is - you enter addresses where you need to drive, you select or create car, you enter how many fuel you have. And after some time(it needs time to compute) you get optimal route with addresses of fuel stations where you need to refuel, cost at this station and gas volume and some additional info about route. Prety easy, isn't it?</Typography>
+        <Typography>So flow is - you enter addresses where you need to drive, you select or create car, you enter how many fuel you have. And after some time(it needs time to compute) you get optimal route with addresses of fuel stations where you need to refuel, cost at this station and gas volume and some additional info about route. Pretty easy, isn't it?</Typography>
       </div>
     )
   }
